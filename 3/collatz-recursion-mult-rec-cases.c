1c155feb1f9df6dbbafc46f888225996381ad851
Slawomir Was <was.slawek@gmail.com>
2023-02-08 23:30:08 +0100

    name-change

---
+++ 3/collatz-recursion-mult-rec-cases.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int collatz(int n);
+
+int main(void)
+{
+    int n = get_int("Number: ");
+    int counter = collatz(n);
+    printf("%i\n", counter);
+}
+
+int collatz(int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    else if (n % 2 == 0)
+    {
+        return 1 + collatz(n / 2);
+    }
+    else
+    {
+        return 1 + collatz(3 * n + 1);
+    }
+
+    return 0;
+}
