6357e4e791a9509cc8289d6ab11bfa71b395ff98
Slawomir Was <was.slawek@gmail.com>
2023-04-25 11:34:18 +0200

    bank python

---
+++ 6/bank/bank.py
@@ -0,0 +1,12 @@
+greeting = input('Greeting: ')
+
+if greeting.lower().strip()[0:5] == 'hello':
+    print('$0')
+
+elif greeting[0].lower().strip() == 'h':
+    print('$20')
+
+else:
+    print('$100')
+
+#Sławek
